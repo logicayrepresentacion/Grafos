5c38417705724f05471ee736c952ebbe4d4923f3
Alejandro Escobar <ealejandro101@gmail.com>
2019-09-24 15:10:41 -0500

    Inicio de ejemplo de recoleccion de basura

---
+++ src/ejemplo/grafos/recoleccionbasura/RecoleccionBasura.java
@@ -0,0 +1,19 @@
+package ejemplo.grafos.recoleccionbasura;
+
+import grafos.matrizadyacencia.matriz.GrafoMatrizAdyacenciaEnMatriz;
+
+/**
+ * RecoleccionBasura
+ */
+public class RecoleccionBasura {
+
+    public static void main(String[] args) {
+        int cantidadVertices = 10;
+        // ToDo - Crear los vertices en un arreglo para poder conocer su cantidad final
+        // ToDo - Cargar los vertices en un arreglo ArrayList leyendo linea a linea
+        GrafoMatrizAdyacenciaEnMatriz gmam = new GrafoMatrizAdyacenciaEnMatriz(cantidadVertices);
+
+        // Recorre nuevamente el archivo y coloca las adyancencias del vertice.
+
+    }
+}
